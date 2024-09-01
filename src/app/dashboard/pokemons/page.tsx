a36d95f8905d36c297208ebf0c4bcf6b786eb6b1
Alexs Ruiz <ralexs.acu@gmail.com>
2024-09-01 12:08:11 -0600

    Extract the pokemons to a Grid component

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,6 +1,6 @@
+import PokemonGrid from "@/app/pokemons/components/PokemonGrid";
 import { PokemonsResponse } from "@/app/pokemons/interfaces/pokemons-response";
 import { SimplePokemon } from "@/app/pokemons/interfaces/simple-pokemon";
-import Image from "next/image";
 
 const getPokemons = async (limit = 20, offset = 0): Promise<SimplePokemon[]> => {
   const data: PokemonsResponse = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
@@ -15,19 +15,8 @@ export default async function PokemonsPage() {
 
   return (
     <div className="flex flex-col">
-     <div className="flex flex-wrap gap-10 items-center justify-center">
-      {
-        pokemons.map((pokemon) => (
-          <Image 
-            key={pokemon.id}
-            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`} 
-            width={100} 
-            height={100}             
-            alt={pokemon.name}
-          />
-        ))
-      }
-     </div>
+      <span className="text-5xl my-4 text-center">Pokemon <small>Static</small> List </span>
+      <PokemonGrid pokemons={pokemons} />
     </div>
   );
 }
