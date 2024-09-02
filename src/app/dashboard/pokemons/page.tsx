5d34aca0dcdd43a880b11338fdf2264d0da793be
Alexs Ruiz <ralexs.acu@gmail.com>
2024-09-02 11:34:29 -0600

    Move the modules out of the app directory

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,21 +1,29 @@
-import PokemonGrid from "@/app/pokemons/components/PokemonGrid";
-import { PokemonsResponse } from "@/app/pokemons/interfaces/pokemons-response";
-import { SimplePokemon } from "@/app/pokemons/interfaces/simple-pokemon";
+import PokemonGrid from '@/pokemons/components/PokemonGrid';
+import { PokemonsResponse } from '@/pokemons/interfaces/pokemons-response';
+import { SimplePokemon } from '@/pokemons/interfaces/simple-pokemon';
 
-const getPokemons = async (limit = 20, offset = 0): Promise<SimplePokemon[]> => {
-  const data: PokemonsResponse = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
-    .then(res => res.json())
+const getPokemons = async (
+  limit = 20,
+  offset = 0
+): Promise<SimplePokemon[]> => {
+  const data: PokemonsResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+  ).then((res) => res.json());
 
-    return data.results.map((pokemon) => ({id: pokemon.url.split('/').at(-2)!, name: pokemon.name}))
-}
+  return data.results.map((pokemon) => ({
+    id: pokemon.url.split('/').at(-2)!,
+    name: pokemon.name
+  }));
+};
 
 export default async function PokemonsPage() {
-
-  const pokemons = await getPokemons(151)
+  const pokemons = await getPokemons(151);
 
   return (
     <div className="flex flex-col">
-      <span className="text-5xl my-4 text-center">Pokemon <small>Static</small> List </span>
+      <span className="text-5xl my-4 text-center">
+        Pokemon <small>Static</small> List{' '}
+      </span>
       <PokemonGrid pokemons={pokemons} />
     </div>
   );
